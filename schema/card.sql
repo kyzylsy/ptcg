c197c9134ca4bf7d8598a995a4b0349d7804518b
Steven Liu <kyzylsy@gmail.com>
2021-09-13 03:01:26 +0800

    add 1ed

--- schema/card.sql
+++ schema/card.sql
@@ -12,7 +12,7 @@ CREATE TABLE `release` (
 	`name`         JSON                                   COMMENT '名称',
 	`abbreviation` VARCHAR(8)                DEFAULT NULL COMMENT '名称缩写',
 	`shortcut`     VARCHAR(8)       NOT NULL              COMMENT '访问索引',
-	`icon`         VARCHAR(256)              DEFAULT NULL COMMENT 'oss存储路径',
+	`logo`         JSON                                   COMMENT 'oss存储路径',
 	`era_id`       INT UNSIGNED     NOT NULL DEFAULT 0    COMMENT '所属世代',
 	`data`         JSON                                   COMMENT '扩展字段',
 	`release_date` BIGINT UNSIGNED  NOT NULL DEFAULT 0    COMMENT '发行日期',
@@ -28,7 +28,8 @@ CREATE TABLE `card` (
 	`type`         VARCHAR(16)      NOT NULL              COMMENT '属性 [POKEMON, TRAINER, ITEM, SUPPORTER, STADIUM, ENERGY]',
     `name`         JSON                                   COMMENT '名称',
     `holofoil`     TINYINT UNSIGNED NOT NULL DEFAULT 0    COMMENT '是否闪 [0: 否, 1: 是]',
-    `icon`         VARCHAR(256)              DEFAULT NULL COMMENT 'oss存储路径',
+    `edition`      TINYINT UNSIGNED NOT NULL DEFAULT 0    COMMENT '是否有1ED [0: 否, 1: 是]',
+    `card`         VARCHAR(256)              DEFAULT NULL COMMENT 'oss存储路径',
     `data`         JSON                                   COMMENT '扩展字段',
     KEY idx_release (release_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '卡牌';
