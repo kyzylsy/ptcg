12bbbe28b5a7569bbe0426dc9ae938fce3aeb1db
Steven Liu <kyzylsy@gmail.com>
2021-09-12 18:49:12 +0800

    bugfix

--- schema/card.sql
+++ schema/card.sql
@@ -7,8 +7,8 @@ CREATE TABLE `era` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '世代';
 
 CREATE TABLE `release` (
-	`id`           INT NSIGNED      NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '商品id',
-	`type`         TINYINT UNSIGNED NOT NULL DEFAULT 0    COMMENT '商品类型 [0: Unknown, 1: MainSet, 2: SubSet, 3: Deck, 4: Promo, 5: Special, 6: Others]'
+	`id`           INT UNSIGNED     NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '商品id',
+	`type`         TINYINT UNSIGNED NOT NULL DEFAULT 0    COMMENT '商品类型 [0: Unknown, 1: MainSet, 2: SubSet, 3: Deck, 4: Promo, 5: Special, 6: Others]',
 	`name`         JSON                                   COMMENT '名称',
 	`abbreviation` VARCHAR(8)                DEFAULT NULL COMMENT '名称缩写',
 	`shortcut`     VARCHAR(8)       NOT NULL              COMMENT '访问索引',
@@ -21,8 +21,8 @@ CREATE TABLE `release` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '商品';
 
 CREATE TABLE `card` (
-	`id`           INT NSIGNED      NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '卡牌id',
-	`release`      INT UNSIGNED     NOT NULL DEFAULT 0    COMMENT '所属商品',
+	`id`           INT UNSIGNED     NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '卡牌id',
+	`release_id`   INT UNSIGNED     NOT NULL DEFAULT 0    COMMENT '所属商品',
 	`index`        VARCHAR(16)               DEFAULT NULL COMMENT '卡牌编号',
 	`mark`         VARCHAR(8)                DEFAULT NULL COMMENT '稀有度标记',
 	`type`         VARCHAR(16)      NOT NULL              COMMENT '属性 [POKEMON, TRAINER, ITEM, SUPPORTER, STADIUM, ENERGY]',
@@ -30,5 +30,5 @@ CREATE TABLE `card` (
     `holofoil`     TINYINT UNSIGNED NOT NULL DEFAULT 0    COMMENT '是否闪 [0: 否, 1: 是]',
     `icon`         VARCHAR(256)              DEFAULT NULL COMMENT 'oss存储路径',
     `data`         JSON                                   COMMENT '扩展字段',
-    KEY idx_release (release)
+    KEY idx_release (release_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '卡牌';
